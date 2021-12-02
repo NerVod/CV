7de0d1dd1092b0f4770a34a803d2a874e4d68d86
NerVod <benjamin.jeannerot@gmail.com>
2021-12-02 21:47:08 +0100

    animation ciinematique in progress

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// supprimer texte intro
+// cacherUnTruc(phraseIntro,6000);
+
+// cacherUnTruc(logoJavaScript, 16100);
+// const cacherUnTruc = function (trucACacher, temporisation){
+    
+// } 
+
+setTimeout(function() {
+    
+    let phraseIntro = document.querySelector('#phraseIntro');
+    phraseIntro.classList.add('cache')
+    
+},6000);
+
+setTimeout(function() {
+    let logoJavaScript = document.querySelector('.javaLogo');
+
+    logoJavaScript.classList.add('cache')
+
+},16100)
