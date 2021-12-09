e1fcb329b2a5e98c7bb96102b0175d3c1321e341
NerVod <benjamin.jeannerot@gmail.com>
2021-12-09 12:58:04 +0100

    Page accueil ok

--- js/script.js
+++ js/script.js
@@ -16,54 +16,191 @@ setTimeout(function() {
 
 // ajout logo occupation espace post animations
 setTimeout(function() {
-    let imagePersistante = document.querySelector('#imagePersistante');
+    var imagePersistante = document.querySelector('#imagePersistante');
     imagePersistante.classList.remove('cache');
     imagePersistante.classList.add('visible');
+    
 
-},38000 );
+},38100 );
 
 
 //  position du div
-let zoneAction = document.querySelector(".droiteSelection");
-let divPhoto = document.getElementById("photoChange");
+// let zoneAction = document.querySelector(".droiteSelection");
+// let divPhoto = document.getElementById("photoChange");
 
-let positionDiv =function() {
-    let rect = divPhoto.getBoundingClientRect();
-    // let divX = rect
-    console.log(rect)
-} 
-zoneAction.addEventListener('mousemove', function(event) {positionDiv()})
+// let positionDiv =function() {
+//     let rect = divPhoto.getBoundingClientRect();
+//     // let divX = rect
+//     console.log(rect)
+// } 
+// zoneAction.addEventListener('mousemove', function(event) {positionDiv()})
 
 
 // photos mobile à la souris
 
 
-var positionSouris = function() {
-    if (addEventListener)
-    var positionSourisX = event.pageX;
-    var positionSourisY = event.pageY;
+// var positionSourisX = event.pageX;
+// var positionSourisY = event.pageY;
+
+// var positionSouris = function() {
+//     if (addEventListener)
+//     positionSourisX = event.pageX;
+//     positionSourisY = event.pageY;
     
-    console.log("positionSourisX " + positionSourisX);
-    console.log("positionSourisY " + positionSourisY);
-}    
+//     console.log("positionSourisX " + positionSourisX);
+//     console.log("positionSourisY " + positionSourisY);
+//     return {
+//         positionSourisX,
+//         positionSourisY
+//     }
+// }    
     
-window.addEventListener('mousemove', function(event) {positionSouris()})
-window.document.attachEvent('onmousemove', positionSouris());
+// let sx = positionSourisX;
+// let sy = positionSourisY
+
+// window.addEventListener('mousemove', function(event) {positionSouris()})
+// window.document.attachEvent('onmousemove', positionSouris());
     
     
 //     position du sprite 
-var imageSprite = document.getElementById("imageSprite") 
-var margeHaut = imageSprite.style.marginTop;
-var margeGauche = imageSprite.style.marginLeft;
+const imageSprite = document.getElementById("imageSprite") 
+const margeHaut = imageSprite.style.marginTop;
+const margeGauche = imageSprite.style.marginLeft;
+
+const zoneHautCentre = document.getElementById('zoneHautCentre');
+const zoneHautDroite = document.getElementById('zoneHautDroite');
+const selectGauche = document.getElementById('selectGauche');
+const selectDroite = document.getElementById('selectDroite');
+const zoneBasGauche = document.getElementById('zoneBasGauche');
+const zoneBasCentre = document.getElementById('zoneBasCentre');
+const zoneBasDroite = document.getElementById('zoneBasDroite');
+
+zoneHautGauche.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.hautGauche.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.hautGauche.image.left + 'px';
+})
+
+zoneHautGauche.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+zoneHautCentre.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.haut.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.haut.image.left + 'px';
+})
+
+zoneHautCentre.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+zoneHautDroite.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.hautDroite.image.top + 'px'
+    imageSprite.style.marginLeft= interpolationSpriteBen.hautDroite.image.left + 'px';
+})
+
+zoneHautDroite.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+selectGauche.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.gauche.image.top + 'px'
+    imageSprite.style.marginLeft= interpolationSpriteBen.gauche.image.left + 'px';
+})
+
+selectGauche.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+selectDroite.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.droite.image.top + 'px'
+    imageSprite.style.marginLeft= interpolationSpriteBen.droite.image.left + 'px';
+})
+
+selectDroite.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+zoneBasGauche.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.basGauche.image.top + 'px'
+    imageSprite.style.marginLeft= interpolationSpriteBen.basGauche.image.left + 'px';
+})
+
+zoneBasGauche.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+zoneBasCentre.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.bas.image.top + 'px'
+    imageSprite.style.marginLeft= interpolationSpriteBen.bas.image.left + 'px';
+})
+
+zoneBasCentre.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+zoneBasDroite.addEventListener('mouseenter', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.basDroite.image.top + 'px'
+    imageSprite.style.marginLeft= interpolationSpriteBen.basDroite.image.left + 'px';
+})
+
+zoneBasDroite.addEventListener('mouseleave', function() {
+    imageSprite.style.marginTop= interpolationSpriteBen.centre.image.top + 'px';
+    imageSprite.style.marginLeft= interpolationSpriteBen.centre.image.left + 'px';
+})
+
+
+
+
 
 var interpolationSpriteBen = {
 
-  bas:  [{
-    margeHaut : 0 + 'px',
-    margeGauche : 0 + 'px' 
-  }],
+  bas :
+  {image:{
+    top:0,
+    left:0
+  }},
+  basDroite :
+  {
+  image:{
+    top:0,
+    left:-242
+  }},
+  basGauche :
+  {image:{
+    top:0,
+    left:-484
+  }},
+  centre :
+  {image:{
+    top:-323,
+    left:0
+  }},
+  droite :
+  {image:{
+    top:-323,
+    left:-241
+  }},
+  gauche :
+  {image:{
+    top:-323,
+    left:-484
+  }},
+  haut :
+  {image:{
+    top:-646,
+    left:0
+  }},
+  hautDroite :
+  {image:{
+    top:-646,
+    left:-242
+  }},
+  hautGauche :
+  {image:{
+    top:-646,
+    left:-484
+  }},
 
 
-}
 
-var spriteBen = interpolationSpriteBen.bas
+};
