d5b22585aa885c3f00c125c637981f3a1f7d55cd
NerVod <benjamin.jeannerot@gmail.com>
2021-12-10 21:04:50 +0100

    deplacement masque div sprite in progress

---
+++ js/game.js
@@ -0,0 +1,772 @@
+var interpolationSpriteObi = {
+    base :[
+    {image:{
+      top:-2010,
+      left:-366,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-402,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-435,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-366,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-402,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-435,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-366,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-402,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-2010,
+      left:-435,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+],
+    avancer :[
+    {image:{
+      top:-3812,
+      left:-366,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3812,
+      left:-402,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3812,
+      left:-436,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3812,
+      left:-471,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3812,
+      left:-504,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3812,
+      left:-538,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3812,
+      left:-573,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3812,
+      left:-608,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-367,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-406,
+    },
+    masque:{
+        height: 68,
+        width: 35,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-444,
+    },
+    masque:{
+        height: 67,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-484,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-521,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-559,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-597,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-634,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3880,
+      left:-672,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-366,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-404,
+    },
+    masque:{
+        height: 67,
+        width: 37,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-482,
+    },
+    masque:{
+        height: 67,
+        width: 39,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-523,
+    },
+    masque:{
+        height: 67,
+        width: 40,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-565,
+    },
+    masque:{
+        height: 67,
+        width: 40,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-606,
+    },
+    masque:{
+        height: 67,
+        width: 39,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-647,
+    },
+    masque:{
+        height: 67,
+        width: 39,
+    }
+},
+    {image:{
+      top:-3945,
+      left:-687,
+    },
+    masque:{
+        height: 67,
+        width: 39,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-365,
+    },
+    masque:{
+        height: 65,
+        width: 39,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-403,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-441,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-479,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-517,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-556,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-593,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-629,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4011,
+      left:-677,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-368,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-408,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-448,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-487,
+    },
+    masque:{
+        height: 65,
+        width: 41,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-531,
+    },
+    masque:{
+        height: 65,
+        width: 41,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-574,
+    },
+    masque:{
+        height: 65,
+        width: 41,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-617,
+    },
+    masque:{
+        height: 65,
+        width: 41,
+    }
+},
+    {image:{
+      top:-4075,
+      left:-659,
+    },
+    masque:{
+        height: 65,
+        width: 41,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-367,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-406,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-444,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-483,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-521,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-559,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-597,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-634,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+    {image:{
+      top:-3882,
+      left:-634,
+    },
+    masque:{
+        height: 65,
+        width: 38,
+    }
+},
+
+
+
+
+
+
+
+],
+    
+
+
+}
+
+/////////////////////////////////////////////////////////////////////////
+///////////////premier jet d'un déplacement du DIV Conteneur du sprite
+///////////////////////////////////////////////////////////////////////
+
+// const containerPersonnage = window.document.getElementById('masqueLuke');
+// const personnage = window.document.getElementById('Luke')
+// const gameArea = window.document.getElementById('gameArea')
+
+// const aDroite = function() {
+
+// if (containerPersonnage.style.marginLeft == ''){
+//     containerPersonnage.style.marginLeft = 0;
+// }
+// containerPersonnage.style.left =  (parseFloat(containerPersonnage.style.left)+1) + 'px';
+
+// };
+// const aGauche = function() {
+//     if (containerPersonnage.style.left == '' || containerPersonnage.style.left == '0px'){
+//         containerPersonnage.style.left = '0px';
+//     } else {
+//         containerPersonnage.style.left =  (parseFloat(containerPersonnage.style.left)-1) + 'px';
+//     } 
+// };
+
+
+// const enHaut = function() {
+//     if (containerPersonnage.style.top == ''){
+//         containerPersonnage.style.top = '0px';
+//         console.log('dans le if');
+//     } else {
+//         containerPersonnage.style.top =  (parseFloat(containerPersonnage.style.top)-1) + 'px';
+//         console.log('dans le else')
+//     } 
+// };
+// const enBas = function() {
+//     if (containerPersonnage.style.top == '') {
+//         containerPersonnage.style.top = '0px';
+//     } else {
+//         containerPersonnage.style.top =  (parseFloat(containerPersonnage.style.top)+1) + 'px';
+//     } 
+// };
+
+
+// window.onkeydown = function(event){
+
+//     var code = event.keyCode;
+
+//     switch(code){
+//       case 37:
+//         //instructions
+//         //alert('gauche');
+//         aGauche();
+//       break;
+//       case 38:
+//         //instructions
+//         // alert('haut');
+//         enHaut()
+//       break;
+//       case 39:
+//         //instructions
+//         //alert('droite');
+//         aDroite();
+//       break;
+//       case 40:
+//         //instructions
+//         // alert('bas');
+//         enBas()
+//       break;
+//       case 13:
+//         //instructions
+//         //alert('entrée');
+//         stopAction();
+//         actionChrono = window.setInterval(enAction,75);;
+//       break;
+//     };
+//   };
+
+//   window.onkeyup = function(){
+//     // auRepos();
+//   };
+
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+var masqueLuke = window.document.getElementById('masqueLuke')
+var gameArea = window.document.getElementById('gameArea')
+
+var deplacementSprite = {
+    directions: {
+      haut: false,
+      droite: false,
+      bas: false,
+      gauche: false
+    },
+    incrementationDePosition: function(position, increment){
+      
+      var valeurDeLaPosition = parseFloat(masqueLuke.style[position]);
+      
+      if (isNaN(valeurDeLaPosition)) {
+        
+        valeurDeLaPosition = 0;
+      }
+      
+      valeurDeLaPosition = valeurDeLaPosition + increment;
+      
+      masqueLuke.style[position] = valeurDeLaPosition + 'px';
+    },
+    leMoteurPourLesAnimations: function(tempsEcoule) {
+      if (this.directions.droite) {
+        this.incrementationDePosition('left', 2);
+      }
+      if (this.directions.gauche) {
+        this.incrementationDePosition('left', -2);
+      }
+      if (this.directions.haut) {
+        this.incrementationDePosition('top', -2);
+      }
+      if (this.directions.bas) {
+        this.incrementationDePosition('top', 2);
+      }
+    },
+    start: function(){
+      var ici = this;
+
+      // sauvegarde code qui marche
+      
+    //   window.addEventListener('keydown', function(evenementSurvenu){
+        
+    //     if ('ArrowRight' === evenementSurvenu.code) {
+    //       ici.directions.droite = true;
+    //     }
+    //     if ('ArrowLeft' === evenementSurvenu.code) {
+    //       ici.directions.gauche = true;
+    //     }
+    //     if ('ArrowUp' === evenementSurvenu.code) {
+    //       ici.directions.haut = true;
+    //     }
+    //     if ('ArrowDown' === evenementSurvenu.code) {
+    //       ici.directions.bas = true;
+    //     }
+    //   });
+
+////////////////////////////////////////////////////////////////////
+/////////////code changé ci dessous///////////////////////////////
+
+    window.addEventListener('keydown', function(evenementSurvenu){
+        
+        var largeurMasque = masqueLuke.offsetWidth;
+        var hauteurMasque = masqueLuke.offsetHeight;
+        
+        var largeurTerrain = gameArea.offsetWidth;
+        var hauteurTerrain = gameArea.offsetHeight;
+        
+        var margeGaucheDeLuke = masqueLuke.offsetLeft;
+        var margeHautDeLuke = masqueLuke.offsetHeight;
+        
+        if ('ArrowRight' === evenementSurvenu.code && largeurTerrain > (largeurMasque + margeGaucheDeLuke)) {
+                ici.directions.droite = true;
+        }
+        if ('ArrowLeft' === evenementSurvenu.code && margeGaucheDeLuke > 0) {
+          ici.directions.gauche = true;
+        }
+        if ('ArrowUp' === evenementSurvenu.code && margeHautDeLuke > 0) {
+          ici.directions.haut = true;
+        }
+        if ('ArrowDown' === evenementSurvenu.code && hauteurTerrain > (hauteurMasque + margeHautDeLuke )) {
+          ici.directions.bas = true;
+        }
+      });
+
+
+/////////////////code changé au dessus //////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+
+
+      window.addEventListener('keyup', function(evenementSurvenu){
+        if ('ArrowRight' === evenementSurvenu.code) {
+          ici.directions.droite = false;
+        }
+        if ('ArrowLeft' === evenementSurvenu.code) {
+          ici.directions.gauche = false;
+        }
+        if ('ArrowUp' === evenementSurvenu.code) {
+          ici.directions.haut = false;
+        }
+        if ('ArrowDown' === evenementSurvenu.code) {
+          ici.directions.bas = false;
+        }
+      });
+
+      window.setInterval(function(){
+        ici.leMoteurPourLesAnimations();
+      },25);
+    }
+  };
+
+  deplacementSprite.start();
+
+  console.dir(deplacementSprite);
+
+  ////////////////////////////////////////////////////
+// var dimensionMasque = masqueLuke.getBoundingClientRect()
+// var largeurMasque = dimensionMasque.width;
+// var hauteurMasque = dimensionMasque.height;
+
+// var dimensionTerrain = gameArea.getBoundingClientRect()
+// var largeurTerrain = dimensionTerrain.width;
+// var hauteurTerrain = dimensionTerrain.height;
+
+// var margeGaucheDeLuke = dimensionMasque.left;
+// var margeHautDeLuke = dimensionMasque.top;
